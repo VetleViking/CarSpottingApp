90db9a7050b2512c191c0de28c6c123cdcd0031b
VetleViking <123548420+VetleViking@users.noreply.github.com>
2024-04-30 10:44:15 +0200

    Update page.tsx

--- csa_next/src/app/makes/selected/page.tsx
+++ csa_next/src/app/makes/selected/page.tsx
@@ -1,20 +1,23 @@
 "use client";
 
 import { get_models } from "@/api/api";
+import Search from "@/components/Search";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export default function MakeSelected() {
+    const [search, setSearch] = useState('');
     const [data, setData] = useState<{ model: string; }[]>([]);
     const searchParams = useSearchParams();
     const make = searchParams.get('make');
-
-    async function fetchData() {
-        const data = await get_models(make as string);
-        setData(data);
-    }
     
     useEffect(() => {
+        const fetchData = async () => {
+            const data = await get_models(make as string, search);
+            
+            setData(data);
+        };
+
         fetchData();
     }, []);
 
@@ -26,6 +29,7 @@ export default function MakeSelected() {
 
     return (
         <div>
+            <Search search={search} setSearch={setSearch} />
             <h1>Selected Make: {make}</h1>
             <h2>Models</h2>
             {data.map((item, id) => (

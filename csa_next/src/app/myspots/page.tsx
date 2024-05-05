4404b600dc09969e1680507e097846e0d68a9b71
VetleViking <vetlelind@gmail.com>
2024-05-05 19:26:19 +0200

    updated myspots page

--- csa_next/src/app/myspots/page.tsx
+++ csa_next/src/app/myspots/page.tsx
@@ -1,16 +1,53 @@
+import { get_makes } from "@/api/api";
+import ListComponent from "@/components/ListComponent";
+import Search from "@/components/Search";
 import Link from "next/link";
+import { useEffect, useState } from "react";
 
 export default function MySpots() {
-  //add api call like the one in makes
+    const [search, setSearch] = useState('');
+    const [data, setData] = useState<{ name: string; }[]>([]);
 
-  
-  return (
-    <div>
-        <div className="text-white text-center">My Spots</div>
-        <div className="text-white">Coming soon</div>  
-        <Link href="/" className="text-white">
-            Back
-        </Link>
-    </div>
+    function selectedMake(make: string) {
+        window.location.href = `/makes/selected?make=${make}`;
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const data = await get_makes(search);
+            
+            setData(data);
+        };
+
+        fetchData();
+    }, [search]);
+
+    if (!data) {
+        return (
+            <div className="text-white">Loading...</div>
+        );
+    }
+
+    return (
+        <div>
+            <p className='text-center text-white text-xl mb-4'>Select the make</p>
+            <Search search={search} setSearch={setSearch} />
+            <div className="text-white text-center">My Spots</div>
+            <Link href="/" className="text-white">
+                Back
+            </Link>
+            <div onClick={() => selectedMake("unknown")}>
+                <ListComponent title="Dont know / other" />
+            </div>
+            {data.map((item, id) => (
+                <div
+                key={id}
+                onClick={() => selectedMake(item.name)}>
+                    <ListComponent  title={item.name} />
+                </div>
+            ))}
+        
+        </div>
+        
   );
 }

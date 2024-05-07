1298dd0d5cce5d126c038b805741df6f6707946d
VetleViking <123548420+VetleViking@users.noreply.github.com>
2024-05-07 14:29:50 +0200

    added api endpoints for makes and models

--- csa_server/src/routes/cars.ts
+++ csa_server/src/routes/cars.ts
@@ -7,14 +7,75 @@ router.get('/makes/:query', async (req: Request, res: Response, next: NextFuncti
     try {
         const { query } = req.params;
         
-        const searchedBefore = await redisClient.hGet('searches', query);
-        if (searchedBefore) {
-            const makes = redisClient.hGet('makes', query);
-            res.status(200).json(makes);
+        const makesObject = await redisClient.hGetAll('makes');
+        const makesArray = Object.keys(makesObject).map(key => makesObject[key]);
+
+        const filteredMakes = makesArray.filter(make => make.includes(query));
+
+        res.status(200).json(filteredMakes);
+        return;
+        
+    } catch(err) {
+        next(err);
+    }
+});
+
+router.get('/makes', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const makesObject = await redisClient.hGetAll('makes');
+        const makesArray = Object.keys(makesObject).map(key => makesObject[key]);
+
+        res.status(200).json(makesArray);
+        return;
+        
+    } catch(err) {
+        next(err);
+    }
+});
+
+router.get('/makes/:make/models/:query', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { make, query } = req.params;
+        
+        const modelsObject = await redisClient.hGet(`make:${make}`, query);
+        const modelsArray = Object.keys(modelsObject).map(key => modelsObject[key]);
+
+        if (modelsArray.length === 0) {
+            res.status(404).json({ message: 'No models found' });
             return;
         }
 
+        res.status(200).json(modelsArray);
+        return;
         
+    } catch(err) {
+        next(err);
+    }
+});
+
+router.get('/makes/unknown/models/:query', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const { query } = req.params;
+        
+        const makesObject = await redisClient.hGetAll('makes');
+        const makesArray = Object.keys(makesObject).map(key => makesObject[key]);
+
+        let modelsArray: string[] = [];
+
+        makesArray.forEach(async make => {
+            const modelsObject = await redisClient.hGet(`make:${make}`, query);
+        
+            const models = Object.keys(modelsObject).map(key => modelsObject[key]);
+            modelsArray = modelsArray.concat(models);
+        });
+
+        if (modelsArray.length === 0) {
+            res.status(404).json({ message: 'No models found' });
+            return;
+        }
+
+        res.status(200).json(modelsArray);
+
     } catch(err) {
         next(err);
     }
@@ -35,6 +96,7 @@ router.post('/makes/:make', async (req: Request, res: Response, next: NextFuncti
 
         res.status(201).json({ message: 'Make created' });
         return;
+
     } catch(err) {
         next(err);
     }

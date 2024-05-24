8d5f1c4f4a186288829628e0634a060b364dde4c
VetleViking <123548420+VetleViking@users.noreply.github.com>
2024-05-24 14:39:41 +0200

    trying to make listcomponent less boring

--- csa_next/src/components/ListComponent.tsx
+++ csa_next/src/components/ListComponent.tsx
@@ -16,16 +16,13 @@ const ListComponent = ({ title, text }: ListProps) => {
     }
     
     return (
-        // <div className="rounded-lg bg-gray-700 px-1 py-2 my-1 mx-2 border-2 border-gray-800 cursor-pointer">
-        //     <p className="text-white">{title}</p>       
-        // </div>
-
-        // <div className="rounded-lg bg-[#e73d2e50] px-1 py-2 my-1 mx-2 border-2 border-[#f39301] cursor-pointer">
-        //     <p className="text-white">{title}</p>       
-        // </div>
-
-        <div className="mx-1 bg-black px-1 py-2 border border-white cursor-pointer">
-            <p className="text-white font-ListComponent">{title}</p>       
+        <div className="mx-1 bg-black border border-white cursor-pointer flex justify-between">
+            <p className="text-white font-ListComponent px-1 py-2">{title}</p>
+            <div className="flex ">       
+                <div className="border-l-[20px] border-l-transparent border-b-[40px] border-r-[20px] mr-[-20px] border-white"></div>
+                <div className="border-l-[20px] border-l-transparent border-r-[20px] border-b-[40px] mr-[-20px] border-[#e72328]"></div>
+                <div className="border-l-[20px] border-l-transparent border-b-[40px] border-r-[10px] border-white"></div>
+            </div>
         </div>
     );
 };

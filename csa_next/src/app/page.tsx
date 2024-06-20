22d697a671ab37a041718c9681d9ef138b652d1d
VetleViking <vetlelind@gmail.com>
2024-06-20 22:01:36 +0200

    temp

--- csa_next/src/app/page.tsx
+++ csa_next/src/app/page.tsx
@@ -2,6 +2,7 @@
 import Header from "@/components/Header";
 import { useState } from "react";
 import { ensure_login } from "@/functions/functions";
+import { upload_makes } from "@/api/api";
 
 export default function Home() {
   const [username, setUsername] = useState('');
@@ -20,6 +21,7 @@ export default function Home() {
       <div className="mt-8 flex flex-col items-center">
           <p className="text-4xl text-white mb-2">Car Spotting App</p>
           <p className="text-xl text-white">Welcome, {username}</p>
+          <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => upload_makes()}>upload</button>
       </div>
     </div>
   );

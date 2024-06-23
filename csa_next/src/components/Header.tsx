6cb3938825cf17a5c5ebbdbe19ef922073410bd6
VetleViking <vetlelind@gmail.com>
2024-06-23 18:06:39 +0200

    updated header to look better on mobile

--- csa_next/src/components/Header.tsx
+++ csa_next/src/components/Header.tsx
@@ -11,7 +11,7 @@ type HeaderProps = {
 const Header = ({ search, setSearch, username }: HeaderProps) => {
     return (
         <div className=" bg-white border-b-[6px] border-white">
-            <div className="flex justify-center py-3 items-center gap-8 lg:gap-20 border-b-[6px] border-black">
+            <div className="flex justify-between lg:px-20 px-4 py-3 items-center border-b-[6px] border-black">
                 <button onClick={() => window.location.href = `/makes?username=${username}`} className="italic font-medium">My <span className="text-[#e72328]">spots</span></button>
                 {setSearch && <Search search={search as string} setSearch={setSearch} />}
                 <button onClick={() => window.location.href = `/makes`} className=" italic font-medium">Upload <span className="text-[#e72328]">spot</span></button>

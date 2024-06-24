641d1399ecb96fa9daa0f203c4157c7b775cfdaa
VetleViking <vetlelind@gmail.com>
2024-06-24 22:44:09 +0200

    debug

--- csa_server/src/routes/cars.ts
+++ csa_server/src/routes/cars.ts
@@ -474,12 +474,17 @@ const sharp = require('sharp');
 
 // function to compress image
 const compressImage = async (base64Image) => {
-    const buffer = Buffer.from(base64Image, 'base64');
-    const compressedBuffer = await sharp(buffer)
-        .resize(384)
-        .jpeg({ quality: 80 }) 
-        .toBuffer();
-    return compressedBuffer.toString('base64');
+    try {
+        const buffer = Buffer.from(base64Image, 'base64');
+        const compressedBuffer = await sharp(buffer)
+            .resize(384)
+            .jpeg({ quality: 80 }) 
+            .toBuffer();
+        return compressedBuffer.toString('base64');
+    } catch (error) {
+        console.error('Error compressing image:', error);
+        throw error;
+    }
 };
 
 router.get('/getspots/:make/:model', async (req: Request, res: Response, next: NextFunction) => {

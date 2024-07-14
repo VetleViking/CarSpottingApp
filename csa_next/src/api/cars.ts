15e0a44e418d6479d6072aa52e92f24061c08285
VetleViking <vetlelind@gmail.com>
2024-07-14 23:41:09 +0200

    Add functionality to add tags to cars

--- csa_next/src/api/cars.ts
+++ csa_next/src/api/cars.ts
@@ -53,6 +53,19 @@ export async function add_model(make: string, model: string) {
     return await response.json();
 }
 
+export async function add_tag(tag: string) {
+    const response = await fetch(`${apiIpCars}addtag`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'authorization': 'Bearer ' + localStorage.getItem('token')
+        },
+        body: JSON.stringify({ tag })
+    });
+
+    return await response.json();
+}
+
 export async function upload_spot(make: string, model: string, images: File[], notes?: string, date?: string) {
     const formData = new FormData();
     formData.append('make', make);

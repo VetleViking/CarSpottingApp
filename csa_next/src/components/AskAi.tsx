6814b7cf317a0b3d5f0381346b01409ba0ea8238
Vetle Lindbråten <vetlelind@gmail.com>
2024-09-13 15:05:40 +0200

    fixed askAi and added sending to spot page

--- csa_next/src/components/AskAi.tsx
+++ csa_next/src/components/AskAi.tsx
@@ -7,7 +7,6 @@ import { add_make, add_model, get_models } from "@/api/cars";
 
 const AskAi = () => {
     const [open, setOpen] = React.useState(false);
-
     const [files, setFiles] = useState<FileList | null>(null);
     const [previewUrls, setPreviewUrls] = useState<string[]>([]);
     const [additional, setAdditional] = useState('');
@@ -28,12 +27,7 @@ const AskAi = () => {
             const text = await imageProcess(base64Data, additional) as CarDetails;
 
             const carExists = await ((text.make !== "cant recognize" && text.model !== "cant recognize") && get_models(text.make, text.model));
-
-            if (carExists.length) {
-                console.log(exists); // debug
-
-                setExists(true);
-            }
+            carExists.length && setExists(carExists.some((car: string) => car.toLowerCase() === text.model.toLowerCase()));
 
             setResults(text);
             console.log(text);
@@ -46,7 +40,9 @@ const AskAi = () => {
         if (!results) return;
 
         add_make(results.make).then(() => {
-            add_model(results.make, results.model);
+            add_model(results.make, results.model).then(() => {
+                window.location.href = `/makes/selected/modelselected?make=${results.make}&model=${results.model}`;
+            })
         });
     }
 
@@ -64,56 +60,50 @@ const AskAi = () => {
     }, [files]);
 
 
-    return (
-        <div className="fixed bottom-0 right-0 bg-black border-t-[6px] border-l-[6px] border-white p-4">
-            {open ? <div className=" flex flex-col gap-2 items-center">
-                <p className="text-white font-ListComponent">Here you can ask AI to identify a car from the chosen image.</p>
-                <div className="flex items-center flex-col max-w-96 gap-2">
-                    <input 
-                        className="rounded-sm bg-black p-1 border border-[#9ca3af] text-[#9ca3af] font-ListComponent" 
-                        type="file" 
-                        accept="image/*" 
-                        onChange={
-                            (e) => {
-                                setFiles(e.target.files);
-                            }}/>
-                        {previewUrls.length && <Spotimage images={previewUrls} />}
-                    <textarea
-                        className="rounded-sm w-full bg-black p-1 mb-2 border border-[#9ca3af] text-[#9ca3af] font-ListComponent"
-                        placeholder="Additional information"
-                        value={additional}
-                        onChange={(e) => setAdditional(e.target.value)}
-                    />
-                </div>
-                {exists ? <Button text="Go to page" onClick={() => {
-                        window.location.href = `/makes/selected/modelselected?make=${results.make}&model=${results.model}`;
-                    }} /> : <Button text="Add to database and go to page" onClick={() => {
-                        uploadMissing().then(() => {
-                            window.location.href = `/makes/selected/modelselected?make=${results.make}&model=${results.model}`;
-                        });
-                    }} />}
-                </div>}
-                <div className="flex justify-between mt-2 w-full">
-                    <Button onClick={() => {
-                        setOpen(false)
-                        setFiles(null)
-                        setPreviewUrls([])
-                        setAdditional('')
-                        setResults(null)
-                        setExists(false)
-                        }} text="Close" />
-                    {loading ? <LoadingAnimation
-                        className="text-base"
-                        text="Asking AI"
-                    /> : <Button onClick={() => upload()} 
-                    text="Ask AI"/>}
-                </div>        
-                
-            </div> : <div >
-                <Button onClick={() => setOpen(true)} text="Ask AI" />
-            </div>}
-        </div>
-    );
+    return <div className="fixed bottom-0 right-0 bg-black border-t-[6px] border-l-[6px] border-white p-4">
+        {open ? <div className=" flex flex-col gap-2 items-center">
+            <p className="text-white font-ListComponent">Here you can ask AI to identify a car from the chosen image.</p>
+            <div className="flex items-center flex-col max-w-96 gap-2">
+                <input 
+                    className="rounded-sm bg-black p-1 border border-[#9ca3af] text-[#9ca3af] font-ListComponent" 
+                    type="file" 
+                    accept="image/*" 
+                    onChange={e => setFiles(e.target.files)}/>
+                    {previewUrls.length && <Spotimage images={previewUrls} />}
+                <textarea
+                    className="rounded-sm w-full bg-black p-1 mb-2 border border-[#9ca3af] text-[#9ca3af] font-ListComponent"
+                    placeholder="Additional information"
+                    value={additional}
+                    onChange={e => setAdditional(e.target.value)}
+                />
+            </div>
+            {exists ? <Button text="Go to page" onClick={() => {
+                    window.location.href = `/makes/selected/modelselected?make=${results?.make}&model=${results?.model}`;
+                }} /> : <Button text="Add to database and go to page" onClick={() => {
+                    uploadMissing().then(() => {
+                        window.location.href = `/makes/selected/modelselected?make=${results?.make}&model=${results?.model}`;
+                    });
+                }} />}
+            <div className="flex justify-between mt-2 w-full">
+                <Button onClick={() => {
+                    setOpen(false)
+                    setFiles(null)
+                    setPreviewUrls([])
+                    setAdditional('')
+                    setResults(null)
+                    setExists(false)
+                    }} text="Close" />
+                {loading ? <LoadingAnimation
+                    className="text-base"
+                    text="Asking AI"
+                /> : <Button onClick={() => upload()} 
+                text="Ask AI"/>}
+            </div>        
+            
+        </div> : <div >
+            <Button onClick={() => setOpen(true)} text="Ask AI" />
+        </div>}
+    </div>
 };
 
 export default AskAi;

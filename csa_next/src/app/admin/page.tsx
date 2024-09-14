ee734ecd98d5e4a44b116a1e2174a10fa4b66c99
VetleViking <vetlelind@gmail.com>
2024-09-14 16:06:50 +0200

    optimizations

--- csa_next/src/app/admin/page.tsx
+++ csa_next/src/app/admin/page.tsx
@@ -7,38 +7,27 @@ import { ensure_login } from '@/functions/functions';
 import Button from '@/components/Button';
 import { check_admin } from '@/api/users';
 import { update_spots } from '@/api/cars';
+import LoadingAnimation from '@/components/LoadingAnim';
 
 function AdminComponent() {
     const [username, setUsername] = useState("");
 
-    if (!username) {
-        ensure_login().then(setUsername);
-    }
+    if (!username) ensure_login().then(setUsername);
 
-    if (username) {
-        check_admin(username).then((res) => {
-            if (!res.is_admin) {
-                window.location.href = '/';
-            }
-        });
-    }
+    if (username) check_admin(username).then(res => {if (!res.is_admin) window.location.href = '/'});
 
-    return (
-        <div>
-            <Header username={username} />
-            <Button
-                onClick={() => update_spots()}
-                text='Update spots'
-                className='text-xl m-4'
-            />
-        </div>
-    );
+    return <div>
+        <Header username={username} />
+        <Button
+            onClick={update_spots}
+            text='Update spots'
+            className='text-xl m-4'
+        />
+    </div>
 }
 
 export default function Admin() {
-    return (
-        <Suspense fallback={<div>Loading...</div>}>
-            <AdminComponent />
-        </Suspense>
-    );
+    return <Suspense fallback={<LoadingAnimation text='Loading'/>}>
+        <AdminComponent />
+    </Suspense>
 };
--- csa_next/src/app/login/page.tsx
+++ csa_next/src/app/login/page.tsx
@@ -5,16 +5,14 @@ import CreateUser from "@/components/CreateUser";
 import LoginComponent from "@/components/LoginComponent";
 
 export default function Login() {
-    return (
-        <div className='flex justify-around flex-wrap gap-12 mt-8 px-8'>
-            <div>
-                <p className="text-white text-center p-2 text-xl italic font-medium">Sign <span className="text-[#e72328]">in</span></p>
-                <LoginComponent />
-            </div>
-            <div>
-                <p className="text-white text-center p-2 text-xl italic font-medium">Sign <span className="text-[#e72328]">up</span></p>
-                <CreateUser />
-            </div>
+    return <div className='flex justify-around flex-wrap gap-12 mt-8 px-8'>
+        <div>
+            <p className="text-white text-center p-2 text-xl italic font-medium">Sign <span className="text-[#e72328]">in</span></p>
+            <LoginComponent />
         </div>
-    );
+        <div>
+            <p className="text-white text-center p-2 text-xl italic font-medium">Sign <span className="text-[#e72328]">up</span></p>
+            <CreateUser />
+        </div>
+    </div>
 }

b327595fa553a8ac64bcddf59348b418367f9cec
VetleViking <vetlelind@gmail.com>
2024-10-27 15:34:24 +0100

    stuffs broken, fix later

--- csa_next/next.config.mjs
+++ csa_next/next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  images: {
+    domains: ['images.vest.li'], // Your image domain here
+  },
 };
 
 export default nextConfig;

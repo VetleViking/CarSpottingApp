946738eb68d17ddd6992ddd20d60742dc62696ad
VetleViking <vetlelind@gmail.com>
2024-12-14 17:46:04 +0100

    small fix

--- csa_next/src/app/admin/AdminClient.tsx
+++ csa_next/src/app/admin/AdminClient.tsx
@@ -24,7 +24,7 @@ export default function AdminClientSide() {
 
     const timeStamp = new Date().getTime();
 
-    return <>
+    return <div>
         <Button
             onClick={update_spots}
             text='Update spots'
@@ -42,43 +42,43 @@ export default function AdminClientSide() {
                 text='Get fix spots'
                 className='text-xl m-4'
             />
-                {spots && spots.length && <div className='flex flex-col gap-2 items-center'>
-                    <p className='text-white text-center text-xl m-4'>{spots[atSpot].make} {spots[atSpot].model}</p>
-                    <div className='flex flex-col gap-2 items-center'>
-                        <p className='text-white text-center'>Spot {atSpot}</p>
-                        <p className='text-white text-center'>Date: {spots[atSpot].date}</p>
-                        <p className='text-white text-center'>Notes: {spots[atSpot].notes}</p>
-                        <p className='text-white text-center'>Tags: {spots[atSpot].tags}</p>
-                        <p className='text-white text-center'>User: {spots[atSpot].user}</p>
-                        <p className='text-white text-center'>Likes: {spots[atSpot].likes}</p>
-                        <p className='text-white text-center'>Upload Date: {spots[atSpot].uploadDate}</p>
-                        <p className='text-white text-center'>Liked by user: {spots[atSpot].likedByUser ? 'Yes' : 'No'}</p>
-                        {spots[atSpot].images && <Spotimage images={spots[atSpot].images.split(', ').map(image => `https://images.vest.li${image}?v=${timeStamp}`)} />}
-                    </div>
-                    <input
-                        className="rounded-sm bg-black p-1 mb-2 border border-[#9ca3af] text-[#9ca3af] font-ListComponent"
-                        type="file"
-                        accept="image/*"
-                        multiple
-                        onChange={e => setFiles(e.target.files)} />
-                    <Spotimage images={files ? Array.from(files).map(file => URL.createObjectURL(file)) : []} />
-                    <Button
-                        onClick={() => {
-                            if (!files) return;
-                            const fileArray = Array.from(files);
-                            fix_spot(spots[atSpot].make, spots[atSpot].model, spots[atSpot].user, spots[atSpot].key, fileArray);
-                            setAtSpot(atSpot + 1);
-                            setFiles(null);
-                        }}
-                        text='Update spot'
-                        className='text-xl m-4'
-                    />
-                    <Button
-                        onClick={() => setAtSpot(atSpot + 1)}
-                        text='Skip'
-                        className='text-xl m-4'
-                    />
-                </div>}
+            {spots && spots.length && <div className='flex flex-col gap-2 items-center'>
+                <p className='text-white text-center text-xl m-4'>{spots[atSpot].make} {spots[atSpot].model}</p>
+                <div className='flex flex-col gap-2 items-center'>
+                    <p className='text-white text-center'>Spot {atSpot}</p>
+                    <p className='text-white text-center'>Date: {spots[atSpot].date}</p>
+                    <p className='text-white text-center'>Notes: {spots[atSpot].notes}</p>
+                    <p className='text-white text-center'>Tags: {spots[atSpot].tags}</p>
+                    <p className='text-white text-center'>User: {spots[atSpot].user}</p>
+                    <p className='text-white text-center'>Likes: {spots[atSpot].likes}</p>
+                    <p className='text-white text-center'>Upload Date: {spots[atSpot].uploadDate}</p>
+                    <p className='text-white text-center'>Liked by user: {spots[atSpot].likedByUser ? 'Yes' : 'No'}</p>
+                    {spots[atSpot].images && <Spotimage images={spots[atSpot].images.split(', ').map(image => `https://images.vest.li${image}?v=${timeStamp}`)} />}
+                </div>
+                <input
+                    className="rounded-sm bg-black p-1 mb-2 border border-[#9ca3af] text-[#9ca3af] font-ListComponent"
+                    type="file"
+                    accept="image/*"
+                    multiple
+                    onChange={e => setFiles(e.target.files)} />
+                <Spotimage images={files ? Array.from(files).map(file => URL.createObjectURL(file)) : []} />
+                <Button
+                    onClick={() => {
+                        if (!files) return;
+                        const fileArray = Array.from(files);
+                        fix_spot(spots[atSpot].make, spots[atSpot].model, spots[atSpot].user, spots[atSpot].key, fileArray);
+                        setAtSpot(atSpot + 1);
+                        setFiles(null);
+                    }}
+                    text='Update spot'
+                    className='text-xl m-4'
+                />
+                <Button
+                    onClick={() => setAtSpot(atSpot + 1)}
+                    text='Skip'
+                    className='text-xl m-4'
+                />
+            </div>}
         </div>
-    </>
+    </div>
 }

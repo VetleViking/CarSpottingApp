0696ecfe75ff9828e30a197c98ac1f36f3d7798a
VetleViking <vetlelind@gmail.com>
2025-07-29 13:27:29 +0200

    removed unused code

--- csa_next/src/components/ReleaseNotes.tsx
+++ csa_next/src/components/ReleaseNotes.tsx
@@ -79,35 +79,6 @@ const ReleaseNotes  = ({ currentVersion }: { currentVersion: string }) => {
         {error && <p className="text-red-500">{error}</p>}
         {message && <p className="text-green-500">{message}</p>}
     </div>
-
-    // Old
-    // const [title, setTitle] = useState<string>("");
-    // const [releaseNotes, setReleaseNotes] = useState<string>("");
-    
-    // const uploadReleaseNotes = async () => {}
-
-    // return <div>
-    //     <input
-    //         className="w-full max-w-2xl mx-1 mb-4 p-1 rounded-lg border border-black font-ListComponent"
-    //         type="text"
-    //         placeholder="Release Title"
-    //         value={title}
-    //         onChange={e => setTitle(e.target.value)}
-    //     />
-    //     <textarea
-    //         className="w-full max-w-2xl mx-1 mb-4 p-1 rounded-lg border border-black font-ListComponent"
-    //         placeholder="Release Notes"
-    //         value={releaseNotes}
-    //         onChange={e => setReleaseNotes(e.target.value)}
-    //         rows={10}
-    //     ></textarea>
-    //     <button
-    //         className="bg-white text-black py-1 px-2 mt-1 italic"
-    //         onClick={uploadReleaseNotes}
-    //     >
-    //         Add Release Notes
-    //     </button>
-    // </div>
 };
 
 export default ReleaseNotes;

283e2195c8e06862140c8f86660b11abcecb9e3f
Vetle Lindbråten <vetlelind@gmail.com>
2025-08-18 11:13:59 +0200

    added cat for 404

--- csa_next/next.config.mjs
+++ csa_next/next.config.mjs
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: false,
   images: {
-    domains: ['images.vest.li'],
+    domains: ['images.vest.li', 'http.cat'],
   },
 };
 
--- csa_next/src/app/not-found.tsx
+++ csa_next/src/app/not-found.tsx
@@ -1,5 +1,6 @@
 import Header from '@/components/Header';
 import { ensure_login } from '@/functions/functions';
+import Image from 'next/image';
 import Link from 'next/link';
 import React from 'react';
 
@@ -9,9 +10,13 @@ export default async function NotFound() {
     return ( 
         <div>
             <Header username={username} />
-            <p className="text-white text-4xl text-center mt-8">
-                404 Page not found
-            </p>
+            <Image
+                src="https://http.cat/images/404.jpg"
+                alt="404 Not Found"
+                width={500}
+                height={500}
+                className="mx-auto mt-8"
+            />
             <div className='flex justify-center mt-8'>
                 <Link href="/" className="bg-[#e72328] text-white p-2 border border-black italic text-nowrap">
                     Go to home page

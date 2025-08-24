6e450ebcc173f08c97edeac187a0895e9139fa1c
Vetle Lindbråten <vetlelind@gmail.com>
2025-08-24 08:57:51 +0200

    updated admin page

--- csa_next/src/app/admin/AdminClient.tsx
+++ csa_next/src/app/admin/AdminClient.tsx
@@ -9,17 +9,19 @@ import AddAdmin from '@/components/AddAdmin';
 
 export default function AdminClientSide({ currentVersion }: { currentVersion: string }) {
     return (
-        <div>
-            <Button
-                onClick={update_spots}
-                text='Update spots'
-                className='text-xl m-4'
-            />
-            <Button
-                onClick={update_users}
-                text='Update users'
-                className='text-xl m-4'
-            />
+        <div className='flex flex-col items-center gap-8'>
+            <div className='flex flex-row justify-center'>
+                <Button
+                    onClick={update_spots}
+                    text='Update spots'
+                    className='text-xl m-4'
+                />
+                <Button
+                    onClick={update_users}
+                    text='Update users'
+                    className='text-xl m-4'
+                />
+            </div>
             <div className='flex flex-col items-center gap-8 m-8'>
                 <AddAdmin />
                 <CreateReleaseNotes currentVersion={currentVersion} />

621bef63e6313fc2415852cb7ee3454e039f81d6
Vetle Lindbråten <vetlelind@gmail.com>
2025-09-27 10:55:20 +0200

    formatted pages

--- csa_next/src/app/makes/MakesClient.tsx
+++ csa_next/src/app/makes/MakesClient.tsx
@@ -11,61 +11,71 @@ import Search from "@/components/Search";
 import Link from "next/link";
 
 interface MakesClientProps {
-    altUsername: string;
-    username?: string;
-};
+	altUsername: string;
+	username?: string;
+}
 
-const MakesClient = ({altUsername, username}: MakesClientProps) => {
-    const [search, setSearch] = useState('');
-    const [data, setData] = useState<{ name: string; }[]>([]);
-    const [loading, setLoading] = useState(true);
+const MakesClient = ({ altUsername, username }: MakesClientProps) => {
+	const [search, setSearch] = useState("");
+	const [data, setData] = useState<{ name: string }[]>([]);
+	const [loading, setLoading] = useState(true);
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const data = username ? await get_spotted_makes(search, username) : await get_makes(search);
-            data.sort((a: string, b: string) => a.localeCompare(b));
-            setData(data);
-            setLoading(false);
-        };
+	useEffect(() => {
+		const fetchData = async () => {
+			const data = username
+				? await get_spotted_makes(search, username)
+				: await get_makes(search);
+			data.sort((a: string, b: string) => a.localeCompare(b));
+			setData(data);
+			setLoading(false);
+		};
 
-        fetchData();
-    }, [search, username]);
+		fetchData();
+	}, [search, username]);
 
-    return (
-        <div>
-            <Header username={altUsername} />
-            <p className='text-center text-white text-3xl my-4'>
-                Select the make
-            </p>
-            <Search search={search} setSearch={setSearch} />
-            <div className='flex gap-2 mx-1 mb-4 flex-wrap md:flex-nowrap'>
-                <div className="bg-black border border-[#9ca3af] cursor-pointer w-full">
-                    <Link href={`/makes/selected?make=unknown${username ? `&username=${username}` : ''}`}>
-                        <p className="text-[#9ca3af] font-ListComponent px-1 py-2 text-nowrap">
-                            Don&apos;t know
-                        </p>
-                    </Link>
-                </div>
-                {!username && <AddNew type='make' />}
-                {!username && <SearchReg />}
-            </div>
-            {Array.isArray(data) && data.length > 0 ? (
-                data.map((item: any, id) => 
-                    <div key={id}>
-                        <Link href={username ? `/makes/selected?make=${item}&username=${username}` : `/makes/selected?make=${item}` }>
-                            <ListComponent title={item} />
-                        </Link>
-                    </div>
-                )
-            ) : loading ? (
-                <LoadingAnimation text="Loading" />
-            ) : (
-                <p className='text-white font-ListComponent px-1 text-nowrap text-center'>
-                    No makes found.
-                </p>
-            )}
-        </div>
-    );
+	return (
+		<div>
+			<Header username={altUsername} />
+			<p className="text-center text-white text-3xl my-4">Select the make</p>
+			<Search search={search} setSearch={setSearch} />
+			<div className="flex gap-2 mx-1 mb-4 flex-wrap md:flex-nowrap">
+				<div className="bg-black border border-[#9ca3af] cursor-pointer w-full">
+					<Link
+						href={`/makes/selected?make=unknown${
+							username ? `&username=${username}` : ""
+						}`}
+					>
+						<p className="text-[#9ca3af] font-ListComponent px-1 py-2 text-nowrap">
+							Don&apos;t know
+						</p>
+					</Link>
+				</div>
+				{!username && <AddNew type="make" />}
+				{!username && <SearchReg />}
+			</div>
+			{Array.isArray(data) && data.length > 0 ? (
+				data.map((item: any, id) => (
+					<div key={id}>
+						<Link
+							href={
+								username
+									? `/makes/selected?make=${item}&username=${username}`
+									: `/makes/selected?make=${item}`
+							}
+						>
+							<ListComponent title={item} />
+						</Link>
+					</div>
+				))
+			) : loading ? (
+				<LoadingAnimation text="Loading" />
+			) : (
+				<p className="text-white font-ListComponent px-1 text-nowrap text-center">
+					No makes found.
+				</p>
+			)}
+		</div>
+	);
 };
 
 export default MakesClient;
--- csa_next/src/app/makes/page.tsx
+++ csa_next/src/app/makes/page.tsx
@@ -1,17 +1,17 @@
-import { ensure_login } from '@/functions/server_functions';
-import MakesClient from './MakesClient';
-import AskAi from '@/components/AskAi';
-import React from 'react';
+import { ensure_login } from "@/functions/server_functions";
+import MakesClient from "./MakesClient";
+import AskAi from "@/components/AskAi";
+import React from "react";
 
-export default async function MakesComponent({searchParams}: SearchParams) {
-    const resolvedSearchParams = await searchParams;
-    const username = resolvedSearchParams.username as string || undefined; 
-    const altUsername = await ensure_login();
+export default async function MakesComponent({ searchParams }: SearchParams) {
+	const resolvedSearchParams = await searchParams;
+	const username = (resolvedSearchParams.username as string) || undefined;
+	const altUsername = await ensure_login();
 
-    return (
-        <div>
-            <AskAi />
-            <MakesClient username={username} altUsername={altUsername} />
-        </div>
-    );
+	return (
+		<div>
+			<AskAi />
+			<MakesClient username={username} altUsername={altUsername} />
+		</div>
+	);
 }

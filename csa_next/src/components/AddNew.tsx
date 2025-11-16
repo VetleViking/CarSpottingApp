912d9a4e5954ebf68a2c793ec7cc71585b33c293
Vetle Lindbråten <vetlelind@gmail.com>
2025-11-16 09:56:44 +0100

    changed some images

--- csa_next/src/components/AddNew.tsx
+++ csa_next/src/components/AddNew.tsx
@@ -2,15 +2,14 @@
 
 import React, { useEffect, useRef, useState } from "react";
 import Button from "./Button";
-import Image from "next/image";
-import down_arrow from "@/images/down_arrow.svg";
 import { add_make, add_model } from "@/api/cars";
 import LoadingAnimation from "./LoadingAnim";
+import DownArrow from "@/images/DownArrow";
 
 interface AddNewProps {
     type: "make" | "model";
     make?: string;
-};
+}
 
 const AddNew = ({ type, make }: AddNewProps) => {
     const [open, setOpen] = useState(false);
@@ -23,12 +22,13 @@ const AddNew = ({ type, make }: AddNewProps) => {
 
         setUploading(true);
         if (type === "make") {
-            add_make(newItem).then(() => 
-                window.location.href = `/makes/selected?make=${newItem}`
+            add_make(newItem).then(
+                () => (window.location.href = `/makes/selected?make=${newItem}`)
             );
         } else if (type === "model" && make) {
-            add_model(make, newItem).then(() => 
-                window.location.href = `/makes/selected/modelselected?make=${make}&model=${newItem}`
+            add_model(make, newItem).then(
+                () =>
+                    (window.location.href = `/makes/selected/modelselected?make=${make}&model=${newItem}`)
             );
         }
     }
@@ -44,36 +44,39 @@ const AddNew = ({ type, make }: AddNewProps) => {
         return () => document.removeEventListener("mousedown", handleClickOutside);
     }, [addNewRef]);
 
-    return <div ref={addNewRef} className="w-full cursor-pointer">
-        <div className="flex justify-between rounded-sm p-1 border border-[#9ca3af]" onClick={() => {
-                setNewItem("");
-                setUploading(false);
-                setOpen(!open);
-            }}>
-            <p className=" font-ListComponent text-[#9ca3af] my-1">Add new {type}</p>
-            <Image 
-                src={down_arrow} alt="Down arrow" 
-                width={15} 
-                height={15} 
-                className={open ? "transform rotate-180" : ""} 
-            />
-        </div>
-        {open && <div className='absolute bg-black border border-[#9ca3af] p-2 w-[calc(100vw-0.5rem)] md:w-auto'>
-            <div className="flex items-center justify-center gap-4">
-                <input
-                    className='font-ListComponent border border-black p-2 w-full h-full rounded-md'
-                    type='text'
-                    placeholder={`New ${type}`}
-                    value={newItem}
-                    onChange={(e) => setNewItem(e.target.value)}
-                />
-                {!uploading 
-                    ? <Button onClick={addNewHandler} text={`Add new ${type}`} /> 
-                    : <LoadingAnimation text="Uploading" />
-                }
+    return (
+        <div ref={addNewRef} className="w-full cursor-pointer">
+            <div
+                className="flex justify-between rounded-sm p-1 border border-[#9ca3af]"
+                onClick={() => {
+                    setNewItem("");
+                    setUploading(false);
+                    setOpen(!open);
+                }}
+            >
+                <p className=" font-ListComponent text-[#9ca3af] my-1">Add new {type}</p>
+                <DownArrow width={15} height={15} className={open ? "transform rotate-180" : ""} />
             </div>
-        </div>}
-    </div>
+            {open && (
+                <div className="absolute bg-black border border-[#9ca3af] p-2 w-[calc(100vw-0.5rem)] md:w-auto">
+                    <div className="flex items-center justify-center gap-4">
+                        <input
+                            className="font-ListComponent border border-black p-2 w-full h-full rounded-md"
+                            type="text"
+                            placeholder={`New ${type}`}
+                            value={newItem}
+                            onChange={(e) => setNewItem(e.target.value)}
+                        />
+                        {!uploading ? (
+                            <Button onClick={addNewHandler} text={`Add new ${type}`} />
+                        ) : (
+                            <LoadingAnimation text="Uploading" />
+                        )}
+                    </div>
+                </div>
+            )}
+        </div>
+    );
 };
 
 export default AddNew;
--- csa_next/src/components/Comments.tsx
+++ csa_next/src/components/Comments.tsx
@@ -2,8 +2,7 @@ import { add_comment, delete_comment, get_comments, like_comment } from "@/api/c
 import { getTimeAgo } from "@/functions/functions";
 import { useState } from "react";
 import Button from "./Button";
-import Image from "next/image";
-import down_arrow from "@/images/down_arrow.svg";
+import DownArrow from "@/images/DownArrow";
 
 interface NestedComment {
     key: string;
@@ -19,32 +18,25 @@ interface NestedComment {
 }
 
 const Comments: React.FC<{
-    username: string, 
-    spotUsername: string, 
-    make: string, 
-    model: string, 
-    spotKey: string,
-    isAdmin: boolean
-}> = ({ 
-    username, 
-    spotUsername, 
-    make, 
-    model, 
-    spotKey,
-    isAdmin
-}) => {
+    username: string;
+    spotUsername: string;
+    make: string;
+    model: string;
+    spotKey: string;
+    isAdmin: boolean;
+}> = ({ username, spotUsername, make, model, spotKey, isAdmin }) => {
     const [open, setOpen] = useState(false);
     const [comments, setComments] = useState<any[]>([]);
-    const [newComment, setNewComment] = useState('');
+    const [newComment, setNewComment] = useState("");
 
     function buildCommentTree(comments: NestedComment[]): NestedComment[] {
         const map: Record<string, NestedComment> = {};
         const roots: NestedComment[] = [];
-      
+
         comments.forEach((c) => {
             map[c.commentId] = { ...c, children: [] };
         });
-      
+
         comments.forEach((c) => {
             if (c.parentId && map[c.parentId]) {
                 map[c.parentId].children?.push(map[c.commentId]);
@@ -56,32 +48,26 @@ const Comments: React.FC<{
         return roots;
     }
 
-    function CommentItem({
-        comment,
-        username,
-    }: {
-        comment: NestedComment;
-        username: string;
-    }) {
+    function CommentItem({ comment, username }: { comment: NestedComment; username: string }) {
         const timeAgo = getTimeAgo(comment.date);
-      
+
         const [liked, setLiked] = useState(comment.liked == "true");
         const [likeCount, setLikeCount] = useState(parseInt(comment.likes) || 0);
-      
+
         const onLike = () => {
             const prevLiked = liked;
             const prevLikeCount = likeCount;
-        
+
             setLiked(!liked);
             setLikeCount(liked ? likeCount - 1 : likeCount + 1);
-        
+
             like_comment(comment.key, comment.commentId).catch((error) => {
                 setLiked(prevLiked);
                 setLikeCount(prevLikeCount);
                 console.error("Error liking comment:", error);
             });
         };
-      
+
         return (
             <div key={comment.commentId} className="px-1 mt-2 border-l-2 border-black">
                 <p>
@@ -96,22 +82,35 @@ const Comments: React.FC<{
                 </p>
                 <p className="break-words">{comment.comment}</p>
                 {comment.deleted != "true" && (
-                <div className="flex gap-2">
-                    <p className="p-1 text-xl text-nowrap">{likeCount} {likeCount === 1 ? 'like' : 'likes'}</p>
-                    <Button text={liked ? 'Remove like' : 'Like'} onClick={onLike} />
-                    <Button text="Reply" onClick={() => onComment(comment.commentId)} />
-                    {isAdmin || username === comment.user && <Button text="Delete" onClick={() => {
-                        delete_comment(spotUsername, make, model, spotKey, comment.commentId).then(() => {
-                            getComments();
-                        });
-                    }} />}
-                </div>
+                    <div className="flex gap-2">
+                        <p className="p-1 text-xl text-nowrap">
+                            {likeCount} {likeCount === 1 ? "like" : "likes"}
+                        </p>
+                        <Button text={liked ? "Remove like" : "Like"} onClick={onLike} />
+                        <Button text="Reply" onClick={() => onComment(comment.commentId)} />
+                        {isAdmin ||
+                            (username === comment.user && (
+                                <Button
+                                    text="Delete"
+                                    onClick={() => {
+                                        delete_comment(
+                                            spotUsername,
+                                            make,
+                                            model,
+                                            spotKey,
+                                            comment.commentId
+                                        ).then(() => {
+                                            getComments();
+                                        });
+                                    }}
+                                />
+                            ))}
+                    </div>
                 )}
                 {comment.children &&
                     comment.children.map((child) => (
                         <CommentItem key={child.commentId} comment={child} username={username} />
-                    ))
-                }
+                    ))}
             </div>
         );
     }
@@ -119,71 +118,75 @@ const Comments: React.FC<{
     const onComment = (parent?: string) => {
         if (!newComment) return;
 
-        add_comment(spotUsername, make, model, spotKey, newComment, parent).then(() => {
-            getComments();
-            setNewComment('');
-        }).catch(error => {
-            setNewComment('');
-            console.error('Error adding comment:', error);
-        });
-    }
+        add_comment(spotUsername, make, model, spotKey, newComment, parent)
+            .then(() => {
+                getComments();
+                setNewComment("");
+            })
+            .catch((error) => {
+                setNewComment("");
+                console.error("Error adding comment:", error);
+            });
+    };
 
     const getComments = () => {
-        get_comments(spotUsername, make, model, spotKey).then((res) => {
-            setComments(res);
-        }).catch(error => {
-            setComments([]);
-            console.error('Error getting comments:', error);
-        });
-    }
+        get_comments(spotUsername, make, model, spotKey)
+            .then((res) => {
+                setComments(res);
+            })
+            .catch((error) => {
+                setComments([]);
+                console.error("Error getting comments:", error);
+            });
+    };
 
     return (
-        <div className='w-full max-w-96 overflow-hidden'>
-            <div 
-                className='flex gap-2 cursor-pointer m-1'
+        <div className="w-full max-w-96 overflow-hidden">
+            <div
+                className="flex gap-2 cursor-pointer m-1"
                 onClick={() => {
-                    if (!open) getComments();    
-                    setOpen(!open)
+                    if (!open) getComments();
+                    setOpen(!open);
                 }}
             >
-                <p className='text-xl'>Comments</p>
-                <Image 
-                    src={down_arrow} 
-                    alt="Down arrow" 
-                    width={15} 
-                    height={15} 
-                    className={open ? "transform rotate-180" : ""} 
-                />
+                <p className="text-xl">Comments</p>
+                <DownArrow width={15} height={15} className={open ? "transform rotate-180" : ""} />
             </div>
-            {open && <div className='bg-white p-1 w-full'>
-                <div className='flex gap-2'>
-                <textarea 
-                    className='w-full p-1 border border-black rounded' 
-                    value={newComment} 
-                    onChange={e => {
-                        e.target.style.height = "auto";
-                        e.target.style.height = e.target.scrollHeight + "px";
-                        if (e.target.value.length > 100) {
-                            e.target.value = e.target.value.slice(0, 100);
-                        }
-                        setNewComment(e.target.value);
-                    }} 
-                    placeholder='Write a comment...'
-                />
-                <Button text='Comment' className="h-min" onClick={() => onComment()} />
+            {open && (
+                <div className="bg-white p-1 w-full">
+                    <div className="flex gap-2">
+                        <textarea
+                            className="w-full p-1 border border-black rounded"
+                            value={newComment}
+                            onChange={(e) => {
+                                e.target.style.height = "auto";
+                                e.target.style.height = e.target.scrollHeight + "px";
+                                if (e.target.value.length > 100) {
+                                    e.target.value = e.target.value.slice(0, 100);
+                                }
+                                setNewComment(e.target.value);
+                            }}
+                            placeholder="Write a comment..."
+                        />
+                        <Button text="Comment" className="h-min" onClick={() => onComment()} />
+                    </div>
+                    <div className="my-2">
+                        {Array.isArray(comments) && comments.length > 0 ? (
+                            buildCommentTree(comments).map((comment) => (
+                                <CommentItem
+                                    key={comment.commentId}
+                                    comment={comment}
+                                    username={username}
+                                />
+                            ))
+                        ) : (
+                            <p className="text-center ">No comments. Be the first to comment!</p>
+                        )}
+                    </div>
                 </div>
-                <div className="my-2">
-                    {Array.isArray(comments) && comments.length > 0 ? (
-                        buildCommentTree(comments).map((comment) => (
-                            <CommentItem key={comment.commentId} comment={comment} username={username} />
-                        ))
-                    ) : (
-                        <p className="text-center ">No comments. Be the first to comment!</p>
-                    )}
-                </div>
-            </div>}
+            )}
         </div>
     );
-}
+};
 
 export default Comments;

de0709eab9cf992fb726ef6afe36874b73df8172
Vetle Lindbråten <vetlelind@gmail.com>
2025-11-22 14:02:42 +0100

    fix button

--- csa_next/src/components/Button.tsx
+++ csa_next/src/components/Button.tsx
@@ -4,13 +4,19 @@ interface ButtonProps {
     text: string;
     onClick: () => void;
     className?: string;
-};
+}
 
 const Button = ({ text, onClick, className }: ButtonProps) => {
-    return <button
-        onClick={() => onClick()}
-        className={className + " bg-[#e72328] text-white p-2 border border-black italic text-nowrap"}
-    >{text}</button>
+    return (
+        <button
+            onClick={() => onClick()}
+            className={
+                className + " bg-[#e72328] text-white p-2 border border-black italic text-nowrap"
+            }
+        >
+            {text}
+        </button>
+    );
 };
 
 export default Button;

d1213128f56b8ee662d0b98a9dc4ef6796f3ecd5
Vetle Lindbråten <vetlelind@gmail.com>
2025-12-19 09:15:40 +0100

    fix

--- csa_next/src/components/Search.tsx
+++ csa_next/src/components/Search.tsx
@@ -11,7 +11,7 @@ const Search = ({ search, setSearch }: SearchProps) => {
     return (
         <div className="flex justify-center">
             <input
-                className={`w-full max-w-2xl mx-1 mb-4 p-1 rounded-lg border border-black font-ListComponent`}
+                className="w-full max-w-2xl mx-1 mb-4 p-1 rounded-lg border border-black font-ListComponent"
                 type="text"
                 placeholder="Search..."
                 value={search}
